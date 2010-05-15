8ab4ce8024315bdcc6986a6cc56fe5782518cee3
Jo De Boeck <deboeck.jo@gmail.com>
2010-05-15 22:38:23 +0300

    Added shutdown menu

--- homeconfig/.config/awesome/myrc/mainmenu.lua
+++ homeconfig/.config/awesome/myrc/mainmenu.lua
@@ -23,6 +23,17 @@ end
 -- Creates main menu
 -- Note: Uses beautiful.icon_theme and beautiful.icon_theme_size
 -- env - table with string constants - command line to different apps
+
+function send_dbus(command, timeout)
+    cli = "dbus-send --system --print-reply --dest=org.freedesktop.Hal \
+    /org/freedesktop/Hal/devices/computer \
+    org.freedesktop.Hal.Device.SystemPowerManagement.".. command
+    if timeout then
+     cli = cli .. " int32:0"
+    end
+    awful.util.spawn(cli)
+end
+
 function build()
 	local terminal = (env.terminal or "terminator") .. " "
 	local man = (env.man or "xterm -e man") .. " "
@@ -33,6 +44,14 @@ function build()
 	freedesktop_utils.icon_theme = "Tango"
 	freedesktop_utils.icon_sizes = {beautiful.icon_theme_size}
 
+    local shutdown_menu = {
+        { "Logout", awesome.quit, freedesktop_utils.lookup_icon({ icon = 'gnome-logout' }) },
+        { "Reboot", function () send_dbus("Reboot", false) end, freedesktop_utils.lookup_icon({ icon = 'gtk-refresh' }) },
+        { "Suspend", function () send_dbus("Suspend", true) end, freedesktop_utils.lookup_icon({ icon = 'xfsm-suspend' }) },
+        { "Hibernate", function () send_dbus("Hibernate", false) end, freedesktop_utils.lookup_icon({ icon = 'xfsm-hibernate' }) },
+        { "Shutdown", function () send_dbus("Shutdown", false) end, freedesktop_utils.lookup_icon({ icon = 'gtk-stop' }) },
+    }
+
 	local mymainmenu_items_head = {
 		{ "Terminal", terminal, freedesktop_utils.lookup_icon({icon = 'terminal'}) },
 		{ "Browser", browser, freedesktop_utils.lookup_icon({icon = 'browser'}) },
@@ -41,8 +60,8 @@ function build()
 
 	local mymainmenu_items_tail = {
 		{"", nil, nil}, --separator
-		{ "Restart", awesome.restart, freedesktop_utils.lookup_icon({ icon = 'gtk-refresh' }) },
-		{ "Quit", awesome.quit, freedesktop_utils.lookup_icon({ icon = 'gnome-logout' }) }
+		{ "Reload", awesome.restart, freedesktop_utils.lookup_icon({ icon = 'gtk-refresh' }) },
+        { "Shutdown", shutdown_menu, freedesktop_utils.lookup_icon({ icon = 'gtk-stop' })}
 	}
 
 	local mymainmenu_items = {}
