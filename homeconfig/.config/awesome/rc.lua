a99a54de44bdd9de7b85ec891189a3a79a49ade5
Jo De Boeck <deboeck.jo@gmail.com>
2010-05-15 20:55:37 +0300

    Better team and link to external modules

--- homeconfig/.config/awesome/rc.lua
+++ homeconfig/.config/awesome/rc.lua
@@ -14,7 +14,7 @@ require("vicious")
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, and wallpapers
-beautiful.init("/usr/share/awesome/themes/zenburn/theme.lua")
+beautiful.init("/usr/share/awesome/themes/default/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 terminal = "terminator"
