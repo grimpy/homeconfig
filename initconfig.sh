0b04ede465d0e4592676d8a0573544dfaa7a920b
Jo De Boeck <deboeck.jo@gmail.com>
2010-09-06 03:25:57 +0300

    improve init

--- initconfig.sh
+++ initconfig.sh
@@ -3,5 +3,8 @@ cd
 mkdir mygit
 cd mygit
 git clone http://github.com/grimpy/homeconfig.git .
+cd homeconfig/.ssh
+unzip id_rsa.zip
+cd ../..
 ./linkfiles.py
 sed -i s#http://github.com/#git@github.com:# .git/config
