28f88c24f7d4e89b23599434098dc44c982352cd
Jo De Boeck <deboeck.jo@gmail.com>
2010-09-21 20:04:51 +0300

    Only run autostart once not every reload

--- homeconfig/.config/awesome/myrc/autostart.lua
+++ homeconfig/.config/awesome/myrc/autostart.lua
@@ -1,12 +1,14 @@
 local ipairs = ipairs
 local awful = require("awful")
 local utils = require("freedesktop.utils")
+local r = require("myrc.runonce")
+
 module("myrc.autostart")
 
 function init(startdir)
     for i, program in ipairs(utils.parse_desktop_files({dir = startdir} )) do
         if program.cmdline then
-            awful.util.spawn_with_shell(program.cmdline)
+            r.run(program.cmdline)
         end
     end
 end
