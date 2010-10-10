a978df646b8fed377c728555e135e475cd85a9ee
Jo De Boeck <deboeck.jo@gmail.com>
2010-10-10 09:31:13 +0200

    Remove some none compatible config in tmux
    Use git:// instead of http:// configure zsh as default shell

--- initconfig.sh
+++ initconfig.sh
@@ -8,8 +8,11 @@ fi
 cd
 mkdir mygit
 cd mygit
-git clone http://github.com/grimpy/homeconfig.git .
+git clone git://github.com/grimpy/homeconfig.git .
 cd homeconfig/.ssh
-unzip id_rsa.zip && sed -i s#http://github.com/#git@github.com:# ../../.git/config
+unzip id_rsa.zip && sed -i s#git://github.com/#git@github.com:# ../../.git/config
 cd ../..
 ./linkfiles.py
+if [ -f /bin/zsh ]; then
+    usermod -s /bin/zsh $USER
+fi
