99d377c0c7705a22884afe701009b5e94bad76bc
Jo De Boeck <deboeck.jo@gmail.com>
2012-03-15 23:44:51 +0200

    Switch back to thunar

--- homeconfig/.config/awesome/myrc/custom.lua
+++ homeconfig/.config/awesome/myrc/custom.lua
@@ -21,7 +21,7 @@ shiftytags = {
     ["2:im"]  = { position = 2, exclusive = true, spawn = "pidgin", screen = VGA, layout = awful.layout.suit.fair.horizontal, },
     ["3:web"]  = { position = 3, exclusive = true, spawn = browser, screen = LCD, layout = awful.layout.suit.max, icon="/usr/share/icons/gnome/16x16/apps/web-browser.png"},
     ["4:mail"]  = { position = 4, exclusive = true, spawn = "thunderbird", layout = awful.layout.suit.max, screen = VGA},
-    ["5:fs"]  = { position = 5, exclusive = true, spawn = "pcmanfm", layout = awful.layout.suit.floating, screen = LCD},
+    ["5:fs"]  = { position = 5, exclusive = true, spawn = "thunar", layout = awful.layout.suit.floating, screen = LCD},
     ["6:edit"]  = { position = 6, exclusive = true, spawn = "gvim", screen = VGA, nopopup = true, layout = awful.layout.suit.max, },
     ["7:media"]  = { position = 7, exclusive = true, screen = LCD, nopopup = true, layout = awful.layout.suit.floating, },
 }
