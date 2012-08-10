969e3c0b4693d745ede0d16a8a3ef2e4574212eb
Jo De Boeck <deboeck.jo@gmail.com>
2012-08-10 23:38:06 +0200

    Added seperators

--- homeconfig/.config/awesome/rc.lua
+++ homeconfig/.config/awesome/rc.lua
@@ -76,6 +76,8 @@ mylauncher = awful.widget.launcher({ image = image(beautiful.awesome_icon),
 mybaticon = widget({ type = "imagebox", name = "mybaticon" })
 mybaticon.image = image(confdir .. "/icons/bat.png")
 mybatwidget     = widget({ type = "textbox", name = "mybatwidget" })
+mysep     = widget({ type = "textbox", name = "mysep" })
+mysep.text = "|"
 mytemp     = widget({ type = "textbox", name = "mytemp" })
 mynet = widget({ type = "textbox" })
 hostname = string.sub(awful.util.pread("hostname"), 0, -2)
@@ -84,8 +86,8 @@ if hostname == "Hulk" or hostname == "wolverine" then
     battery = "BAT1"
 end
 vicious.register(mybatwidget, vicious.widgets.bat, "$1$2%", 61, battery)
-vicious.register(mytemp, vicious.widgets.thermal, "$1°C ", 61, "thermal_zone0")
-vicious.register(mynet, vicious.contrib.net, '<span color="#CC9393">${total down_kb}</span> <span color="#7F9F7F">${total up_kb}</span> ', 3)
+vicious.register(mytemp, vicious.widgets.thermal, "$1°C", 61, "thermal_zone0")
+vicious.register(mynet, vicious.contrib.net, '<span color="#CC9393">${total down_kb}</span> <span color="#7F9F7F">${total up_kb}</span>', 3)
 
 -- {{{ Wibox
 -- Create a textclock widget
@@ -164,10 +166,10 @@ for s = 1, screen.count() do
             layout = awful.widget.layout.horizontal.leftright
         },
         mylayoutbox[s],
-        mytextclock,
-        mynet,
-        mytemp,
-        mybaticon, mybatwidget,
+        mytextclock,mysep,
+        mynet,mysep,
+        mytemp,mysep,
+        mybaticon, mybatwidget,mysep,
         s == 1 and mysystray or nil,
         mytasklist[s],
         layout = awful.widget.layout.horizontal.rightleft
