c5fb52ec68d59403938e58357a626dbe9787a598
Jo De Boeck <deboeck.jo@gmail.com>
2012-11-10 19:38:33 +0200

    Use python-keybinder instead of some crapily documented X library

--- source/pyvolumed/pyvolume.py
+++ source/pyvolumed/pyvolume.py
@@ -1,21 +1,8 @@
 #!/usr/bin/env python
 import alsaaudio
-import xcb, xcb.xproto
+import keybinder
 import pynotify
 import gtk
-XCB_GRAB_MODE_ASYNC = 1
-UP_ARROW = 111
-DOWN_ARROW = 116
-ZERO_KEY = 19
-
-MUTE = 121
-VOL_DOWN = 122
-VOL_UP=123
-
-
-METAMOD = xcb.xproto.ModMask._4
-NUMLOCK = xcb.xproto.ModMask._2
-CAPSLOCK = xcb.xproto.ModMask.Lock
 
 def generatePixBuf(percentage, mute=False):
     HEIGHT= 10
@@ -31,9 +18,38 @@ def generatePixBuf(percentage, mute=False):
     return pixbuf
 
 
+class Mixer(object):
+    def __init__(self, mixer):
+        self._mixer = mixer
+        self._maxvolume = 100
+
+    @property
+    def rawmixer(self):
+        return alsaaudio.Mixer(self._mixer)
+
+    def get_volume(self):
+        return self.rawmixer.getvolume()[0]
+
+    def set_volume(self, volume):
+        mix = self.rawmixer
+        vol = mix.getvolume()[0] + volume
+        if vol > self._maxvolume:
+            vol = self._maxvolume
+        elif vol < 0:
+            vol = 0
+        mix.setvolume(vol)
+        return vol
+
+    def get_mute(self):
+        return self.rawmixer.getmute()[0]
+
+
+    def set_mute(self, mute):
+        return self.rawmixer.setmute(mute)
+
+
 MIXER = 'Master'
-mixer = alsaaudio.Mixer(MIXER)
-maxvolumescale = 100
+mixer = Mixer(MIXER)
 
 nt = None
 
@@ -47,20 +63,8 @@ def getNotification():
 
 getNotification()
 
-def getVolume():
-    return mixer.getvolume()[0]
-
-def newVolume(delta):
-    vol = getVolume() + delta
-    if vol > maxvolumescale:
-        vol = maxvolumescale
-    elif vol < 0:
-        vol = 0
-    mixer.setvolume(vol)
-    return vol
-
 def getNotificationIcon(vol):
-    if mixer.getmute()[0]:
+    if mixer.get_mute():
         return "audio-volume-muted"
     if vol == 0:
         return "audio-volume-off"
@@ -79,12 +83,10 @@ def createbar(percentage):
 
 def notifyDelta(delta, retry=3):
     try:
-        vol = newVolume(delta)
+        vol = mixer.set_volume(delta)
         nt.set_hint_int32("value",vol)
-        muted = mixer.getmute()[0]
-        mutestring = "" if not muted else "Muted"
+        muted = mixer.get_mute()
         nt.set_icon_from_pixbuf(generatePixBuf(vol, muted))
-        #nt.update(" ","\n%s Volume %s %d%%" % (MIXER, mutestring, vol))
         nt.show()
     except Exception, e:
         print e
@@ -94,38 +96,17 @@ def notifyDelta(delta, retry=3):
             notifyDelta(delta, retry)
 
 
-def toggleMute():
-    value = mixer.getmute()[0]
-    mixer.setmute(not value)
+def toggleMute(_):
+    value = mixer.get_mute()
+    mixer.set_mute(not value)
     notifyDelta(0)
 #format = "llHHi"
 
-def registerKey(con, rootwin, mods, mymod, key):
-    for mod in mods:
-        mod = mod | mymod
-        cookie = con.core.GrabKeyChecked(True, rootwin, mod, key, XCB_GRAB_MODE_ASYNC, XCB_GRAB_MODE_ASYNC)
-        if cookie.check():
-            raise Exception("Could not connect to X")
-
 def main():
-    con = xcb.connect()
-    setup = con.get_setup()
-    rootwin = setup.roots[0].root
-    mods = (NUMLOCK, CAPSLOCK, 0, NUMLOCK | CAPSLOCK)
-    for key in (UP_ARROW, DOWN_ARROW, ZERO_KEY):
-        registerKey(con, rootwin, mods, METAMOD, key)
-    for key in (VOL_DOWN, VOL_UP, MUTE):
-        registerKey(con, rootwin, mods, 0, key)
-
-    while True:
-        event = con.wait_for_event()
-        if isinstance(event, xcb.xproto.KeyReleaseEvent):
-            if event.detail in (UP_ARROW, VOL_UP):
-                notifyDelta(+5)
-            elif event.detail in (DOWN_ARROW, VOL_DOWN):
-                notifyDelta(-5)
-            elif event.detail in (ZERO_KEY, MUTE):
-                toggleMute()
+    keybinder.bind('<Super>Up', notifyDelta, 5)
+    keybinder.bind('<Super>Down', notifyDelta, -5)
+    keybinder.bind('<Super>0', toggleMute, None)
+    gtk.mainloop()
 
 
 if '__main__' == __name__:
