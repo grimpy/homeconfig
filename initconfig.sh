badb0bed1ef08d55b55c072b802f4a3613fd5cc4
Jo De Boeck <deboeck.jo@gmail.com>
2012-12-01 09:50:10 +0200

    Make initconfig.sh cleaner

--- initconfig.sh
+++ initconfig.sh
@@ -5,14 +5,13 @@ if [ "x$remoteip" != "x" ]; then
 	ssh $remoteip /tmp/initconfig.sh
 	exit 0
 fi
-cd
-mkdir mygit
-cd mygit
-git clone git://github.com/grimpy/homeconfig.git .
-cd homeconfig/.ssh
-sed -i s#git://github.com/#git@github.com:# ../../.git/config
-cd ../..
+pushd ~ > /dev/null
+git clone git://github.com/grimpy/homeconfig.git mygit
+pushd mygit > /dev/null
+sed -i s#git://github.com/#git@github.com:# .git/config
 ./linkfiles.py
+popd > /dev/null
 if [ -f /bin/zsh ]; then
     usermod -s /bin/zsh $USER
 fi
+popd > /dev/null
