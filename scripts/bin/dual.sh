e92c7b6c90acc0ca6ff1642e127bd90a23c0f212
Jo De Boeck <deboeck.jo@gmail.com>
2013-01-31 22:01:41 +0200

    Remove some obsolete scripts

--- scripts/bin/dual.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-OTHER=HDMI1
-if [ -n "$1" ]; then
-    OTHER=$1
-fi
-xrandr --output $OTHER --auto
-xrandr --output LVDS1 --left-of $OTHER --auto
--- scripts/bin/single.sh
+++
@@ -1,1 +0,0 @@
-xrandr --output HDMI1 --off
