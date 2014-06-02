6b141ecad96a34ef044881eed337ddfcb1093a31
Jo De Boeck <deboeck.jo@gmail.com>
2014-06-02 15:21:41 +0300

    First day of the week is sunday

--- homeconfig/.config/awesome/cal.lua
+++ homeconfig/.config/awesome/cal.lua
@@ -87,7 +87,7 @@ function cal.register(mywidget, custom_current_day_format)
                 function tooltip:update()
                         local month, year = os.date('%m'), os.date('%Y')
                         state = {month, year}
-                        tooltip:set_markup(string.format('<span font_desc="'.. device.font ..'">%s</span>', displayMonth(month, year, 2)))
+                        tooltip:set_markup(string.format('<span font_desc="'.. device.font ..'">%s</span>', displayMonth(month, year, 1)))
                 end
                 tooltip:update()
 	end
@@ -124,7 +124,7 @@ end
 
 function switchMonth(delta)
 	state[1] = state[1] + (delta or 1)
-	local text = string.format('<span font_desc="' .. device.font .. '">%s</span>', displayMonth(state[1], state[2], 2))
+	local text = string.format('<span font_desc="' .. device.font .. '">%s</span>', displayMonth(state[1], state[2], 1))
 	tooltip:set_markup(text)
 end
 
