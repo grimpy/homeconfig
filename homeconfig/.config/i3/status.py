f9bf30fb1abc11c889120dca0e393f4a6ca9959d
Jo De Boeck <deboeck.jo@gmail.com>
2015-12-15 21:12:59 +0200

    Change the plugins to use

--- homeconfig/.config/i3/status.py
+++ homeconfig/.config/i3/status.py
@@ -1,18 +1,13 @@
 # coding=utf-8
 from i3pystatus import Status
 import psutil
+import netifaces
 
 status = Status(standalone=True)
 
-# Displays clock like this:
-# Tue 30 Jul 11:59:46 PM KW31
-#                          ^-- calendar week
 status.register("clock",
-        format="%a %-d %b %H:%M KW%V",)
+        format=" %a %-d %b %H:%M KW%V",)
 
-# Shows pulseaudio default sink volume
-#
-# Note: requires libpulseaudio from PyPI
 status.register("pulseaudio",
     format="♪{volume}",)
 
@@ -21,12 +16,11 @@ status.register('now_playing',
                 'pause': '',
                 'stop': ''})
 
-# Shows your CPU temperature, if you have a Intel CPU
 status.register("temp",
     format="{temp:.0f}°C",)
 
-status.register("cpu_usage_graph", format='{cpu_graph}', graph_width=5)
-status.register("mem_bar", format='{used_mem_bar}')
+status.register("cpu_usage_bar", format=' {usage_bar}', bar_type='vertical')
+status.register("mem", format=' {used_mem} / {total_mem} GiB', divisor=1024**3)
 
 status.register("battery",
     format="{status} {percentage:.0f}% {remaining:%E%hh:%Mm}",
@@ -38,43 +32,25 @@ status.register("battery",
         "FULL": "⚡",
     },)
 
-# Shows the address and up/down state of eth0. If it is up the address is shown in
-# green (the default value of color_up) and the CIDR-address is shown
-# (i.e. 10.10.10.42/24).
-# If it's down just the interface name (eth0) will be displayed in red
-# (defaults of format_down and color_down)
-#
-# Note: the network module requires PyPI package netifaces
 status.register("network",
     interface="bond0",
     color_up='#FFFFFF',
     on_leftclick='sudo systemctl restart dhcpcd',
     format_up="{v4}",)
 
-# Note: requires both netifaces and basiciw (for essid and quality)
-status.register("network",
-    interface="wlan0",
-    on_leftclick='wpa_gui',
-    color_up='#FFFFFF',
-    format_up="{essid}",)
+wfaces = list(filter(lambda x: x.startswith('w'), netifaces.interfaces()))
+if wfaces:
+    status.register("network",
+        interface=wfaces[0],
+        on_leftclick='wpa_gui',
+        color_up='#FFFFFF',
+        format_up="{essid}",)
 
 status.register("network",
     interface="bond0",
     divisor=1024,
     start_color='white',
     format_up=" {bytes_recv}K  {bytes_sent}K",)
-
-
-# Shows mpd status
-# Format:
-# Cloud connected▶Reroute to Remain
-#status.register("mpd",
-#    format="{title}{status}{album}",
-#    status={
-#        "pause": "▷",
-#        "play": "▶",
-#        "stop": "◾",
-#    },)
 
 status.run()
 
