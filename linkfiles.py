098e514f77de0eb29ee0b026b6d0e008102af7aa
Jo De Boeck <deboeck.jo@gmail.com>
2016-02-08 20:38:43 +0200

    Support for overlay

--- linkfiles.py
+++ linkfiles.py
@@ -7,8 +7,9 @@ parser = OptionParser()
 parser.add_option("-r", "--root", dest="root", action="store_true", default=False,
                   help="Do root")
 (options, args) = parser.parse_args()
-fullpath = os.path.abspath(os.path.dirname(sys.argv[0]))
-linkfolders = ['.config/awesome', '.config/autostart', '.zsh.d', '.ssh', '.vim', '.urxvt', '.config/i3', '.config/i3blocks']
+repopath = os.path.abspath(os.path.dirname(sys.argv[0]))
+fullpaths = []
+linkfolders = ['.config/awesome', '.config/autostart', '.vim', '.urxvt', '.config/i3', '.config/i3blocks']
 copydirs = ['etc/udev/rules.d']
 target = os.environ['HOME']
 
@@ -17,19 +18,22 @@ if options.root:
         print "Use sudo not root"
         sys.exit(1)
     else:
-        fullpath = os.path.join(fullpath, 'osconfig')
+        fullpaths.append(os.path.join(repopath, 'osconfig'))
+        overlay = os.path.join(fullpath, 'overlay', 'osconfig')
+        if os.path.exists(overlay):
+            fullpaths.append(os.path.join(repopath, 'osconfig'))
         target = "/"
 else:
-    fullpath = os.path.join(fullpath, 'homeconfig')
+    fullpaths.append(os.path.join(repopath, 'homeconfig'))
+    overlay = os.path.join(repopath, 'overlay', 'homeconfig')
+    if os.path.exists(overlay):
+        fullpaths.append(overlay)
 
-striplen = len(fullpath) + 1
 #do folders defined in linkfolders
 #cleanuppath
 def cleanuppath(targetfolder):
     if os.path.islink(targetfolder):
         os.remove(targetfolder)
-    elif os.path.isdir(targetfolder):
-        shutil.rmtree(targetfolder)
     elif os.path.exists(targetfolder):
         os.remove(targetfolder)
 #createParentFolder
@@ -41,25 +45,26 @@ def createParentFolder(targetfolder):
     parenttarget = os.path.abspath(os.path.join(targetfolder, '..'))
     createRecusiveFolder(parenttarget)
 
-for curdir, dirs, files in os.walk(fullpath):
-#do normal files in root
-    relativedir = curdir[striplen:]
-    targetdir = os.path.join(target, relativedir)
-    if relativedir not in linkfolders:
-        print 'Creating links files for folder', relativedir
-        createRecusiveFolder(targetdir)
-        for file_ in files:
-            targetfile = os.path.join(targetdir, file_)
-            sourcefile = os.path.join(curdir, file_)
-            cleanuppath(targetfile)
-            if relativedir in copydirs:
-                shutil.copy(sourcefile, targetfile)
-            else:
-                os.symlink(sourcefile, targetfile)
-    else:
-        cleanuppath(targetdir)
-        createParentFolder(targetdir)
-        print 'Link dir', relativedir
-        os.symlink(curdir, targetdir)
-        for dir_ in dirs[::-1]:
-            dirs.remove(dir_)
+for mypath in fullpaths:
+    striplen = len(mypath) + 1
+    for curdir, dirs, files in os.walk(mypath):
+    #do normal files in root
+        relativedir = curdir[striplen:]
+        targetdir = os.path.join(target, relativedir)
+        if relativedir not in linkfolders:
+            print 'Creating links files for folder', relativedir
+            createRecusiveFolder(targetdir)
+            for file_ in files:
+                targetfile = os.path.join(targetdir, file_)
+                sourcefile = os.path.join(curdir, file_)
+                cleanuppath(targetfile)
+                if relativedir in copydirs:
+                    shutil.copy(sourcefile, targetfile)
+                else:
+                    os.symlink(sourcefile, targetfile)
+        else:
+            cleanuppath(targetdir)
+            createParentFolder(targetdir)
+            os.symlink(curdir, targetdir)
+            for dir_ in dirs[::-1]:
+                dirs.remove(dir_)
