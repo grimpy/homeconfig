4b325abf98d7280e2a1cad8a788c06fb0edc1e05
Jo De Boeck <deboeck.jo@gmail.com>
2016-03-05 15:43:07 +0200

    I3: tool for noticing urgent events

---
+++ source/i3events/i3events.py
@@ -0,0 +1,23 @@
+import i3
+import os
+import sys
+import subprocess
+
+def workspacechange(ws, wsp, *args):
+    if ws['change'] == 'urgent':
+        urgent = False
+        for ws in wsp:
+            urgent |= ws['urgent']
+        if urgent:
+            subprocess.Popen(['scrolllock'], shell=True)
+        else:
+            if os.path.exists('/tmp/scrolllock'):
+                os.unlink('/tmp/scrolllock')
+        print urgent
+
+socketpath = i3.get_socket_path()
+if not os.path.exists(socketpath):
+    print 'I3 is not running'
+    sys.exit(3)
+
+i3.Subscription(workspacechange, 'workspace')
