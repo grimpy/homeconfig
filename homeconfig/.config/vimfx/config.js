c1aaf2ae523e555e96ef9e41db7dcaf52eaa421b
Jo De Boeck <deboeck.jo@gmail.com>
2017-02-12 10:22:38 +0100

    Correct facebook domain
    Signed-off-by: Jo De Boeck <deboeck.jo@gmail.com>

--- homeconfig/.config/vimfx/config.js
+++ homeconfig/.config/vimfx/config.js
@@ -1,3 +1,3 @@
 vimfx.addKeyOverrides([location => location.hostname === 'feedly.com', ['j', 'k', 'v']],
-                      [location => location.hostname === 'facebook.com', ['j', 'k']],
+                      [location => location.hostname === 'www.facebook.com', ['j', 'k']],
                       [location => location.hostname === 'plus.google.com', ['j', 'k']]);
