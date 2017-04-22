379733c6685be89eed28b62860ad96048bc31e7e
Jo De Boeck <deboeck.jo@gmail.com>
2017-04-22 12:00:22 +0200

    Some changes to i3 status

--- homeconfig/.config/i3/status.py
+++ homeconfig/.config/i3/status.py
@@ -1,20 +1,42 @@
 # coding=utf-8
-from i3pystatus import Status
-import psutil
-import subprocess
+from i3pystatus import Status, IntervalModule
 import netifaces
 
-def spawn(*args):
-    def wrapper(self):
-        subprocess.Popen(args)
-    return wrapper
+
+class VodaFone(IntervalModule):
+    interval = 3600
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        try:
+            import vodafone
+            self.vf = vodafone.Vodafone()
+        except ImportError:
+            self.vf = None
+
+    def run(self):
+        if self.vf is None:
+            return
+        self.vf.parse()
+        daypercent = (self.vf.dayused / self.vf.daylimit) * 100
+        nightpercent = (self.vf.nightused / self.vf.nightlimit) * 100
+        remainingpercent = (self.vf.daysleft / self.vf.totaldays) * 100
+        if remainingpercent < daypercent or remainingpercent < nightpercent:
+            color = "#FF0000"
+        else:
+            color = "#00FF00"
+        self.data = self.vf.__dict__
+        self.output = {"full_text": "{day:.0f}% {night:.0f}% {remaining}".format(day=daypercent, night=nightpercent, remaining=self.vf.daysleft),
+                       'color': color}
+
+    def refresh(self):
+        self.run()
+
+
 
 status = Status(standalone=True)
 
-#clock.Clock.on_leftclick = ""
 status.register("clock",
-        on_leftclick=[spawn('xterm', '-class', 'Float', '-geometry', '74x9', '-e', 'cal -3 -w; read')],
-        #on_leftclick=[['wpa_gui']],
+        on_rightclick='mycal',
         format=" %a %-d %b %H:%M")
 
 status.register("pulseaudio",
@@ -31,11 +53,12 @@ status.register("temp",
 status.register("cpu_usage_bar",
                 format='<span color="#FFFFFF"></span> {usage_bar}',
                 hints = {"markup": "pango"},
-                on_leftclick=[spawn('xterm', '-class', 'Float', '-geometry', '120x40', '-e', 'htop')],
+                on_rightclick='xterm -rv -class Float -geometry 120x40 -e htop',
                 bar_type='vertical'
                 )
 status.register("mem_bar",
                 multi_colors=True,
+                on_rightclick='xterm -rv -class Float -geometry 120x40 -e htop',
                 format='{used_mem_bar}')
 
 status.register("battery",
@@ -49,17 +72,19 @@ status.register("battery",
         "FULL": "⚡",
     },)
 
+
 status.register("network",
     interface="bond0",
     color_up='#FFFFFF',
-    on_leftclick=[spawn('sudo', 'systemctl', 'restart', 'dhcpcd')],
+    on_rightclick='sudo systemctl restart dhcpcd',
     format_up="{v4}",)
 
+status.register(VodaFone, on_leftclick="refresh", interval=3600)
 wfaces = list(filter(lambda x: x.startswith('w'), netifaces.interfaces()))
 if wfaces:
     status.register("network",
         interface=wfaces[0],
-        on_leftclick=[spawn('wpa_gui', '-i', 'wlan0')],
+        on_rightclick='wpa_gui -i {}'.format(wfaces[0]),
         color_up='#FFFFFF',
         format_up="{essid}",)
 
@@ -67,6 +92,7 @@ status.register("network",
     interface="bond0",
     divisor=1024,
     start_color='white',
+    on_rightclick="xterm -rv -class Float -geometry 150x50 -e 'sudo jnettop -i bond0'",
     format_up=" {bytes_recv}K  {bytes_sent}K",)
 
 status.register("file",
