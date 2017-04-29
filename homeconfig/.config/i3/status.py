ef2aface7f984c1dd357b10915a7e3f5c7b2ee95
Jo De Boeck <deboeck.jo@gmail.com>
2017-04-29 16:37:26 +0200

    Reformat my i3status
    Signed-off-by: Jo De Boeck <deboeck.jo@gmail.com>

--- homeconfig/.config/i3/status.py
+++ homeconfig/.config/i3/status.py
@@ -1,5 +1,6 @@
 # coding=utf-8
 from i3pystatus import Status, IntervalModule
+from i3pystatus.group import Group
 import netifaces
 
 
@@ -19,86 +20,78 @@ class VodaFone(IntervalModule):
         self.vf.parse()
         daypercent = (self.vf.dayused / self.vf.daylimit) * 100
         nightpercent = (self.vf.nightused / self.vf.nightlimit) * 100
-        remainingpercent = (self.vf.daysleft / self.vf.totaldays) * 100
+        remainingpercent = 100 - (self.vf.daysleft / self.vf.totaldays) * 100
         if remainingpercent < daypercent or remainingpercent < nightpercent:
             color = "#FF0000"
         else:
-            color = "#00FF00"
+            color = "#000000"
         self.data = self.vf.__dict__
-        self.output = {"full_text": "{day:.0f}% {night:.0f}% {remaining}".format(day=daypercent, night=nightpercent, remaining=self.vf.daysleft),
+        self.output = {"full_text": " D{day:.0f}% N{night:.0f}% R{remaining:.0f}%".format(day=daypercent, night=nightpercent, remaining=remainingpercent),
                        'color': color}
 
     def refresh(self):
         self.run()
 
 
-
-status = Status(standalone=True)
-
+status = Status(standalone=True, logfile='$HOME/.cache/i3pystatus.log')
 status.register("clock",
-        on_rightclick='mycal',
-        format=" %a %-d %b %H:%M")
-
+               on_rightclick='mycal',
+               format=" %a %-d %b %H:%M")
 status.register("pulseaudio",
-    format="♪{volume}", bar_type="horizontal",
-    multi_colors=True)
-
+                format="♪{volume}", bar_type="horizontal",
+                multi_colors=True)
 status.register('now_playing',
                 status={'play': '',
-                'pause': '',
-                'stop': ''})
-
-status.register("temp",
-    format=" {temp:.0f}°C",)
-
+                        'pause': '',
+                        'stop': ''})
+status.register("temp", format=" {temp:.0f}°C",)
 status.register("cpu_usage_bar",
                 format='<span color="#FFFFFF"></span> {usage_bar}',
                 hints = {"markup": "pango"},
                 on_rightclick='xterm -class Float -geometry 120x40 -e htop',
                 bar_type='vertical'
                 )
-status.register("mem_bar",
-                multi_colors=True,
+status.register("mem",
                 on_rightclick='xterm -class Float -geometry 120x40 -e htop',
-                format='{used_mem_bar}')
-
+                color='#FFFFFF',
+                hints = {"markup": "pango"},
+                format='<span color="#FFFFFF"></span> {percent_used_mem}%')
 status.register("battery",
-    format='<span color="#FFFFFF">{status}</span> {percentage:.0f}% <span color="#FFFFFF">{remaining:%E%hh:%Mm}</span> ',
-    hints = {"markup": "pango"},
-    alert=True,
-    alert_percentage=5,
-    status={
-        "DIS": "",
-        "CHR": "",
-        "FULL": "⚡",
-    },)
+                format='<span color="#FFFFFF">{status}</span> {percentage:.0f}% <span color="#FFFFFF">{remaining:%E%hh:%Mm}</span>',
+                hints = {"markup": "pango"},
+                alert=True,
+                alert_percentage=5,
+                status={
+                       "DIS": "",
+                       "CHR": "",
+                       "FULL": "⚡",
+                })
+group = Group()
+group.register("network",
+               interface="bond0",
+               color_up='#FFFFFF',
+               on_rightclick='sudo systemctl restart dhcpcd',
+               format_up="{v4}",)
 
-
-status.register("network",
-    interface="bond0",
-    color_up='#FFFFFF',
-    on_rightclick='sudo systemctl restart dhcpcd',
-    format_up="{v4}",)
-
-status.register(VodaFone, on_leftclick="refresh", interval=3600)
 wfaces = list(filter(lambda x: x.startswith('w'), netifaces.interfaces()))
 if wfaces:
-    status.register("network",
-        interface=wfaces[0],
-        on_rightclick='wpa_gui -i {}'.format(wfaces[0]),
-        color_up='#FFFFFF',
-        format_up="{essid}",)
+    group.register("network",
+                   interface=wfaces[0],
+                   on_rightclick='wpa_gui -i {}'.format(wfaces[0]),
+                   color_up='#FFFFFF',
+                   format_up="{essid}",)
 
-status.register("network",
-    interface="bond0",
-    divisor=1024,
-    start_color='white',
-    on_rightclick="xterm -class Float -geometry 150x50 -e 'sudo jnettop -i bond0'",
-    format_up=" {bytes_recv}K  {bytes_sent}K",)
-
-status.register("file",
-        components={'bond': (str, '/sys/class/net/bond0/bonding/active_slave')},
-        format="{bond}")
+group.register("network",
+               interface="bond0",
+               divisor=1024,
+               start_color='white',
+               on_rightclick="xterm -class Float -geometry 150x50 -e 'sudo jnettop -i bond0'",
+               format_up=" {bytes_recv}K  {bytes_sent}K",)
+group.register("file",
+               components={'bond': (str, '/sys/class/net/bond0/bonding/active_slave')},
+               format="{bond}")
+status.register(group)
+status.register(VodaFone, on_leftclick="refresh", interval=3600)
 
 status.run()
 
