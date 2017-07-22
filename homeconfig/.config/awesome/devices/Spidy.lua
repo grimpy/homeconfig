a8c17cf6c395246f15e993269c839fb40ac33224
Jo De Boeck <deboeck.jo@gmail.com>
2017-07-22 17:13:28 +0200

    Fix loading of device specific config

--- homeconfig/.config/awesome/devices/Spidy.lua
+++ homeconfig/.config/awesome/devices/Spidy.lua
@@ -1,5 +1,4 @@
 device = {}
-device.font = "Monospace 14"
 device.battery = "BAT1"
 device.amixer = "Master"
 
--- homeconfig/.config/awesome/myrc/device.lua
+++ homeconfig/.config/awesome/myrc/device.lua
@@ -1,8 +1,10 @@
 local os = os
+local io = io
 local pcall = pcall
 local dofile = dofile
 local pairs = pairs
 local awful = require('awful')
+local mylogger = require('mylogger')
 
 module("myrc.device")
 
@@ -11,13 +13,16 @@ function trim(s)
   return (s:gsub("^%s*(.-)%s*$", "%1"))
 end
 
-local hostname = trim(awful.util.pread("hostname -s"))
+local file = io.popen('hostname -s', 'r')
+local hostname = trim(file:read('*all'))
+file:close()
 local configdir = awful.util.getdir("config")
 devicepath = configdir .. "/devices/defaults.lua" 
 success, device = pcall(function() return dofile(devicepath) end)
 
 -- loa device specific
 local devicepath = configdir .. "/devices/" .. hostname .. ".lua"
+mylogger.log('device', devicepath)
 if awful.util.file_readable(devicepath) then
     local success, mydevice = pcall(function() return dofile(devicepath) end)
     for k,v in pairs(mydevice) do device[k] = v end
