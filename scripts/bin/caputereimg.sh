49005f3957cb3b3b30ef27e497c3d66c44436f0f
Jo De Boeck <deboeck.jo@gmail.com>
2019-01-02 22:30:46 +0200

    Add support for wayland to screenshot

--- scripts/bin/caputereimg.sh
+++ scripts/bin/caputereimg.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-if [ -d "$1" ]; then
-    timestamp=`date +%y%m%d_%H%M%S`
-    maim -s -k -u ${1}/${timestamp}.png
-    echo -n "${1}/${timestamp}.png" | xclip -selection clipboard
-else 
-    maim -s -k -u |  xclip -selection clipboard -t image/png
+if [ -n "$WAYLAND_DISPLAY" ]; then
+    grim -g "$(slurp)" - | wl-copy
+else
+    if [ -d "$1" ]; then
+        timestamp=`date +%y%m%d_%H%M%S`
+        maim -s -k -u ${1}/${timestamp}.png
+        echo -n "${1}/${timestamp}.png" | xclip -selection clipboard
+    else 
+        maim -s -k -u |  xclip -selection clipboard -t image/png
+    fi
 fi
