5081a87e2c587fe5704fef4bc88dcbd8e5d746b9
Jo De Boeck <deboeck.jo@gmail.com>
2020-05-12 23:04:44 +0200

    Improve i3 status
    Signed-off-by: Jo De Boeck <deboeck.jo@gmail.com>

---
+++ homeconfig/.config/i3/pulse.py
@@ -0,0 +1,371 @@
+import threading
+import subprocess
+import logging
+
+from i3pystatus import Module, IntervalModule
+from i3pystatus.core.color import ColorRangeModule
+import pulsectl
+import dbus
+import time
+import socket
+import struct
+
+
+class BoseConnect:
+    HANDSHAKE = b"\x00\x01\x01\x00"
+    HANDSHAKE_ACK = b"\x00\x01\x03\x05"
+    BATERY = b"\x02\x02\x01\x00"
+    BATERY_ACK = b"\x02\x02\x03\x01"
+
+    def __init__(self, address):
+        self._address = address
+        self._sock = None
+        try:
+            self.connect()
+        except OSError:
+            pass
+
+    def connect(self):
+        if self._sock:
+            self._sock.close()
+        self._sock = socket.socket(
+            socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM
+        )
+        self._sock.connect((self._address, 8))
+        self.command(self.HANDSHAKE, self.HANDSHAKE_ACK, 5)
+
+    def _command(self, msg, ack, responselength):
+        self._sock.sendall(msg)
+        assert self._sock.recv(len(ack)) == ack
+        return self._sock.recv(responselength)
+
+    def command(self, msg, ack, responselength):
+        try:
+            return self._command(msg, ack, responselength)
+        except (OSError, AssertionError):
+            self.connect()
+            return self._command(msg, ack, responselength)
+
+    def get_battery(self):
+        try:
+            info = self.command(self.BATERY, self.BATERY_ACK, 1)
+            return struct.unpack("b", info)[0]
+        except (OSError, AssertionError):
+            return -1
+
+
+class Pulser:
+    def __init__(self):
+        self._pulse = None
+        self._callbacks = []
+        self.logger = logging.getLogger("pulser")
+        self.logger.setLevel(logging.DEBUG)
+        self._started = False
+        self.lock = threading.Lock()
+
+    @property
+    def api(self):
+        if self._pulse is None:
+            self._pulse = pulsectl.Pulse()
+        if not self._pulse.connected:
+            self._pulse.connect()
+        return self._pulse
+
+    def __enter__(self):
+        self.lock.acquire()
+        return self.api
+
+    def __exit__(self, *args):
+        self.lock.release()
+
+    def add_callback(self, func):
+        self._callbacks.append(func)
+
+    def start(self):
+        if not self._started:
+            threading.Thread(target=self.listen).start()
+            self._started = True
+        self.run_callbacks()
+
+    def run_callbacks(self):
+        for callback in self._callbacks:
+            try:
+                callback()
+            except Exception as e:
+                self.logger.error("failed to execute callback", exc_info=e)
+
+    @property
+    def current_sink(self):
+        with self as api:
+            return self._get_best_dev(api.sink_list())
+
+    @property
+    def current_mic(self):
+        with self as api:
+            return self._get_best_dev(api.source_list())
+
+    def _get_best_dev(self, devs):
+        bdev = None
+        for dev in devs:
+            if dev.description.startswith("Monitor"):
+                continue
+            if dev.state == pulsectl.PulseStateEnum.running:
+                return dev
+            if bdev is None:
+                bdev = dev
+            if "Built-in" not in dev.description:
+                bdev = dev
+        return bdev
+
+    def listen(self):
+        proc = subprocess.Popen(["pactl", "subscribe"], stdout=subprocess.PIPE)
+        while True:
+            event = proc.stdout.readline().decode()
+            if 'on client' in event:
+                continue
+            self.run_callbacks()
+            if proc.poll() is not None:
+                time.sleep(5)
+                proc = subprocess.Popen(["pactl", "subscribe"], stdout=subprocess.PIPE)
+
+
+class PulseMic(Module):
+    settings = ("format",)
+    format = "{icon}"
+    on_rightclick = "switch_mute"
+
+    def __init__(self, *args, pulser=None, **kwargs):
+        self.pulser = pulser or Pulser()
+        super().__init__(*args, **kwargs)
+
+    def init(self):
+        self.pulser.add_callback(self.update)
+        self.pulser.start()
+
+    def update(self):
+        """Updates self.output"""
+        mic = self.pulser.current_mic
+
+        if mic.mute:
+            micicon = ""
+            miccolor = "#ff0000"
+        else:
+            micicon = ""
+            miccolor = "#ffffff"
+        if mic.state == pulsectl.PulseStateEnum.running:
+            miccolor = "#00ff00"
+
+        self.output = {
+            "color": miccolor,
+            "full_text": self.format.format(icon=micicon,),
+        }
+
+        self.send_output()
+
+    def switch_mute(self):
+        mic = self.pulser.current_mic
+        with self.pulser as api:
+            if mic.mute:
+                api.source_mute(mic.index, False)
+            else:
+                api.source_mute(mic.index, True)
+
+
+class PulseBluetooth(IntervalModule):
+    settings = ("format", "address")
+    format = "{icon} {name} {bat}%"
+    icon = ""
+    interval = 600
+    address = "04:52:C7:0A:5E:0B"
+    on_rightclick = "switch_profile"
+    on_leftclick = "connect"
+
+    def __init__(self, *args, pulser=None, **kwargs):
+        self.pulser = pulser or Pulser()
+        self.battery = 0
+        self.lastupdate = 0
+        super().__init__(*args, **kwargs)
+
+    def init(self):
+        self.pulser.add_callback(self.update)
+        self.pulser.start()
+        self.bose = BoseConnect(self.address)
+
+    def update(self):
+        """Updates self.output"""
+        name = ""
+        card = self.get_bluetooth_card()
+        if card:
+            if self.lastupdate + 300 < time.time():
+                self.lastupdate = time.time()
+                bat = self.bose.get_battery()
+                if bat != -1:
+                    self.battery = bat
+            name = card.proplist["device.description"]
+            if card.profile_active.name == "headset_head_unit":
+                color = "#00ff00"
+            else:
+                color = "#0080ff"
+        else:
+            color = "#ffffff"
+
+        self.output = {
+            "color": color,
+            "full_text": self.format.format(
+                icon=self.icon, name=name, bat=self.battery
+            ),
+        }
+
+        self.send_output()
+
+    def run(self):
+        self.update()
+
+    def get_profile_by_name(self, card, name, fallback=None):
+        fallback_profile = None
+        for profile in card.profile_list:
+            if profile.available:
+                if profile.name.startswith(name):
+                    self.logger.warning(f"Returning {profile.name}")
+                    return profile
+                elif fallback and profile.name.startswith(fallback):
+                    fallback_profile = profile
+        if fallback_profile:
+            self.logger.warning(f"Returning {profile.name}")
+
+        return fallback_profile
+
+    def switch_profile(self):
+        card = self.get_bluetooth_card()
+        if not card:
+            return
+        if card.profile_active.name.startswith("a2dp_sink"):
+            newprofile = self.get_profile_by_name(card, "headset_head_unit")
+        else:
+            newprofile = self.get_profile_by_name(card, "a2dp_sink_aac", "a2dp_sink")
+        if newprofile:
+            with self.pulser as api:
+                api.card_profile_set(card, newprofile)
+
+    def get_bluetooth_card(self):
+        with self.pulser as api:
+            for card in api.card_list():
+                if card.name.startswith("bluez_card"):
+                    return card
+
+    def connect(self):
+        address = self.address.replace(":", "_")
+        objectpath = f"/org/bluez/hci0/dev_{address}"
+        bus = dbus.SystemBus()
+        obj = bus.get_object("org.bluez", objectpath)
+        interface = dbus.Interface(obj, "org.bluez.Device1")
+        interface.Connect()
+
+
+class PulseAudio(Module, ColorRangeModule):
+    """
+    Shows volume of default PulseAudio sink (output).
+
+    - Requires amixer for toggling mute and incrementing/decrementing volume on scroll.
+    - Depends on the PyPI colour module - https://pypi.python.org/pypi/colour/0.0.5
+
+    .. rubric:: Available formatters
+
+    * `{volume}` — volume in percent (0...100)
+    * `{db}` — volume in decibels relative to 100 %, i.e. 100 % = 0 dB, 50 % = -18 dB, 0 % = -infinity dB
+      (the literal value for -infinity is `-∞`)
+    * `{muted}` — the value of one of the `muted` or `unmuted` settings
+    * `{volume_bar}` — unicode bar showing volume
+    """
+
+    settings = (
+        "format",
+        ("format_muted", "optional format string to use when muted"),
+        "muted",
+        "unmuted",
+        "color_muted",
+        "color_unmuted",
+        ("step", "percentage to increment volume on scroll"),
+        ("sink", "sink name to use, None means pulseaudio default"),
+        (
+            "multi_colors",
+            "whether or not to change the color from "
+            "'color_muted' to 'color_unmuted' based on volume percentage",
+        ),
+    )
+
+    muted = "M"
+    unmuted = ""
+    format = "♪: {volume}"
+    format_muted = None
+    color_muted = "#FF0000"
+    color_unmuted = "#FFFFFF"
+
+    step = 5
+    multi_colors = False
+
+    on_rightclick = "switch_mute"
+    on_doubleleftclick = "change_sink"
+    on_leftclick = "pavucontrol"
+    on_upscroll = "increase_volume"
+    on_downscroll = "decrease_volume"
+
+    def __init__(self, *args, pulser=None, **kwargs):
+        self.pulser = pulser or Pulser()
+        super().__init__(*args, **kwargs)
+
+    def init(self):
+        self.colors = self.get_hex_color_range(
+            self.color_muted, self.color_unmuted, 100
+        )
+        self.pulser.add_callback(self.update)
+        self.pulser.start()
+
+    def update(self):
+        """Updates self.output"""
+        sink = self.pulser.current_sink
+        volume_percent = round(100 * sink.volume.value_flat)
+        muted = sink.mute
+
+        if self.multi_colors and not muted:
+            color = self.get_gradient(volume_percent, self.colors)
+        else:
+            color = self.color_muted if muted else self.color_unmuted
+
+        if muted and self.format_muted is not None:
+            output_format = self.format_muted
+        else:
+            output_format = self.format
+
+        if muted:
+            icon = ""
+        else:
+            icon = ""
+
+        self.output = {
+            "color": color,
+            "full_text": output_format.format(
+                muted=muted, volume=volume_percent, icon=icon
+            ),
+        }
+
+        self.send_output()
+
+    def switch_mute(self):
+        sink = self.pulser.current_sink
+        with self.pulser as api:
+            if sink.mute:
+                api.sink_mute(sink.index, False)
+            else:
+                api.sink_mute(sink.index, True)
+
+    def increase_volume(self):
+        sink = self.pulser.current_sink
+        with self.pulser as api:
+            api.volume_change_all_chans(sink, 0.05)
+
+    def decrease_volume(self):
+        sink = self.pulser.current_sink
+        # subprocess.run(["pactl", "set-sink-volume", sink.name, "-5%"])
+        with self.pulser as api:
+            api.volume_change_all_chans(sink, -0.05)
--- homeconfig/.config/i3/status.py
+++ homeconfig/.config/i3/status.py
@@ -4,6 +4,8 @@ from i3pystatus.group import Group
 import netifaces
 import os
 from isp_status import TelecomEgypt, VodaFone
+import pulse
+
 
 class KernelStatus(IntervalModule):
     def run(self):
@@ -14,91 +16,115 @@ class KernelStatus(IntervalModule):
         self.output = {"full_text": msg, "color": "#FF0000"}
 
 
-status = Status(standalone=True, logfile='$HOME/.cache/i3pystatus.log')
+status = Status(standalone=True, logfile="$HOME/.cache/i3pystatus.log")
 dategroup = Group()
-dategroup.register("clock",
-               on_rightclick='mycal',
-               on_leftclick='rofi -modi "Clock:roficlock" -show Clock',
-               format="%H:%M")
-dategroup.register("clock",
-               on_rightclick='mycal',
-               format="%a %-d %b %Y")
+dategroup.register(
+    "clock",
+    on_rightclick="mycal",
+    on_leftclick='rofi -modi "Clock:roficlock" -show Clock',
+    format="%H:%M",
+)
+dategroup.register("clock", on_rightclick="mycal", format="%a %-d %b %Y")
 status.register(dategroup)
-status.register("pulseaudio",
-                format="♪{volume}", bar_type="horizontal",
-                multi_colors=True)
-status.register("mpd",
+
+pulser = pulse.Pulser()
+status.register(
+    pulse.PulseAudio,
+    pulser=pulser,
+    format='{icon}{volume}',
+    multi_colors=True,
+)
+status.register(
+    pulse.PulseMic,
+    pulser=pulser,
+    format='{icon}',
+)
+status.register(
+    pulse.PulseBluetooth,
+    pulser=pulser,
+    format='{icon} {bat}%',
+)
+status.register(
+    "mpd",
+    password="dummy",
     on_leftclick="switch_playpause",
     on_rightclick=["mpd_command", "stop"],
     on_middleclick=["mpd_command", "shuffle"],
     on_upscroll=["mpd_command", "seekcur -10"],
-    status={'play': '',
-            'pause': '',
-            'stop': ''},
-    on_downscroll=["mpd_command", "seekcur +10"])
-status.register('now_playing',
-                status={'play': '',
-                        'pause': '',
-                        'stop': ''})
-status.register("temp", format="{temp:.0f}°C",)
-status.register("cpu_usage_bar",
-                format='<span color="#FFFFFF"></span> {usage_bar}',
-                hints = {"markup": "pango"},
-                on_rightclick='ftop',
-                bar_type='vertical'
-                )
-status.register("mem",
-                on_rightclick='ftop',
-                color='#FFFFFF',
-                hints = {"markup": "pango"},
-                format='<span color="#FFFFFF"></span> {percent_used_mem}%')
-status.register("battery",
-                format='<span color="#FFFFFF">{status}</span>{percentage:.0f}%<span color="#FFFFFF"> {remaining:%E%hh:%Mm}</span>',
-                hints = {"markup": "pango"},
-                alert=True,
-                alert_percentage=5,
-                full_color="#FFFFFF",
-                status={
-                       "DIS": "",
-                       "CHR": "",
-                       "FULL": "⚡",
-                })
+    status={"play": "", "pause": "", "stop": ""},
+    on_downscroll=["mpd_command", "seekcur +10"],
+)
+status.register("now_playing", status={"play": "", "pause": "", "stop": ""})
+status.register(
+    "temp", format="{temp:.0f}°C",
+)
+status.register(
+    "cpu_usage_bar",
+    format='<span color="#FFFFFF"></span> {usage_bar}',
+    hints={"markup": "pango"},
+    on_rightclick="ftop",
+    bar_type="vertical",
+)
+status.register(
+    "mem",
+    on_rightclick="ftop",
+    color="#FFFFFF",
+    hints={"markup": "pango"},
+    format='<span color="#FFFFFF"></span> {percent_used_mem}%',
+)
+status.register(
+    "battery",
+    format='<span color="#FFFFFF">{status}</span>{percentage:.0f}%<span color="#FFFFFF"> {remaining:%E%hh:%Mm}</span>',
+    hints={"markup": "pango"},
+    alert=True,
+    alert_percentage=5,
+    full_color="#FFFFFF",
+    status={"DIS": "", "CHR": "", "FULL": "⚡",},
+)
 group = Group()
-group.register("network",
-               interface="bond0",
-               divisor=1024,
-               start_color='white',
-               on_rightclick="mynet",
-               format_up="{bytes_recv}K {bytes_sent}K",)
-group.register("network",
-               interface="bond0",
-               color_up='#FFFFFF',
-               on_rightclick='sudo systemctl restart dhcpcd',
-               format_up="{v4:.<7}",)
+group.register(
+    "network",
+    interface="bond0",
+    divisor=1024,
+    start_color="white",
+    on_rightclick="mynet",
+    format_up="{bytes_recv}K {bytes_sent}K",
+)
+group.register(
+    "network",
+    interface="bond0",
+    color_up="#FFFFFF",
+    on_rightclick="sudo systemctl restart dhcpcd",
+    format_up="{v4:.<7}",
+)
 
-wfaces = list(filter(lambda x: x.startswith('w'), netifaces.interfaces()))
+wfaces = list(filter(lambda x: x.startswith("w"), netifaces.interfaces()))
 if wfaces:
-    group.register("network",
-                   interface=wfaces[0],
-                   on_rightclick='wpa_gui -i {}'.format(wfaces[0]),
-                   color_up='#FFFFFF',
-                   format_up="{essid}",)
+    group.register(
+        "network",
+        interface=wfaces[0],
+        on_rightclick="wpa_gui -i {}".format(wfaces[0]),
+        color_up="#FFFFFF",
+        format_up="{essid}",
+    )
 
 status.register(group)
 
-def bond_icon(data):
-    if data.startswith(('en', 'eth')):
-        return ''
-    else:
-        return ''
 
-status.register("file",
-               components={'bond': (bond_icon, '/sys/class/net/bond0/bonding/active_slave')},
-               format="{bond}")
+def bond_icon(data):
+    if data.startswith(("en", "eth")):
+        return ""
+    else:
+        return ""
+
+
+status.register(
+    "file",
+    components={"bond": (bond_icon, "/sys/class/net/bond0/bonding/active_slave")},
+    format="{bond}",
+)
 status.register(VodaFone, on_leftclick="refresh", interval=3601)
 status.register(TelecomEgypt, on_leftclick="refresh", interval=3600)
 status.register(KernelStatus, interval=3600)
 
 status.run()
-
-
